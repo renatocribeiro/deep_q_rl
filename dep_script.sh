451a822fdc627654d88612ea47187044a23555de
Jakub Sygnowski <sygnowski@gmail.com>
2016-04-14 14:11:30 +0200

    repository info

--- dep_script.sh
+++ dep_script.sh
@@ -41,7 +41,7 @@ echo "==>installing ALE ..."
 # dependencies ...
 sudo apt-get install libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake
 
-git clone https://github.com/mgbellemare/Arcade-Learning-Environment ALE
+git clone https://github.com/sygi/Arcade-Learning-Environment ALE
 cd ./ALE
 cmake -DUSE_SDL=ON -DUSE_RLGLUE=OFF .
 make -j2
