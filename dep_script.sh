0d14fba23831a639fb42cfbd751877269c05c1f8
micha7a <micha7a@gmail.com>
2016-04-24 18:30:17 +0200

    repair dependency script

--- dep_script.sh
+++ dep_script.sh
@@ -13,7 +13,7 @@ sudo apt-get install python-matplotlib python-tk
 
 echo "==>installing Theano ..."
 # some dependencies ...
-sudo apt-get install python-numpy python-scipy python-dev python-pip python-nose g++ libopenblas-dev git
+sudo apt-get install python-numpy python-scipy python-dev python-pip python-nose g++ libopenblas-dev git python-yaml
 pip install --user --upgrade --no-deps git+git://github.com/Theano/Theano.git
 
 echo "==>installing Lasagne ..."
@@ -34,19 +34,15 @@ cd ./pylearn2
 python setup.py develop --user
 cd ..
 
-if [ ! -d "./ALE" ]
-then
 echo "==>installing ALE ..."
-
 # dependencies ...
 sudo apt-get install libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake
 
-git clone https://github.com/sygi/Arcade-Learning-Environment ALE
+git clone https://github.com/mgbellemare/Arcade-Learning-Environment ALE
 cd ./ALE
 cmake -DUSE_SDL=ON -DUSE_RLGLUE=OFF .
 make -j2
 pip install --user .
 cd ..
-fi
 
 echo "==>All done!"
