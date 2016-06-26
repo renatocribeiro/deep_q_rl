00add171f2d9e8392c334dc88faa0caafbfc4bdd
Jakub Sygnowski <sygnowski@gmail.com>
2016-06-26 18:19:37 +0200

    simple corrections

--- agent/train.py
+++ agent/train.py
@@ -25,9 +25,9 @@ import numpy as np
 import theano
 import lasagne
 from lasagne.layers import InputLayer, DenseLayer
+import matplotlib.pyplot as plt
 
 obs_layer = InputLayer(observation_shape, name="observation layer")
-# TODO: scale down
 
 hidden_layer_1 = lasagne.layers.DenseLayer(obs_layer,
                                            num_units=RAM_SIZE,
@@ -45,6 +45,7 @@ from agentnet.memory import RNNCell
 hidden_state_size = 32
 prev_state_layer = InputLayer([None, hidden_state_size], name='prev_state_layer')
 rnn = RNNCell(prev_state_layer, hidden_layer_2, name='rnn')
+
 q_vals = DenseLayer(hidden_layer_2, num_units=n_actions,
                     nonlinearity=lasagne.nonlinearities.linear,
                     name="QValues")
@@ -57,7 +58,7 @@ resolver = EpsilonGreedyResolver(q_vals, epsilon=epsilon, name="resolver")
 
 from agentnet.agent import Agent
 agent = Agent(obs_layer,
-              agent_states={rnn: prev_state_layer},
+              agent_states={},  # ={rnn: prev_state_layer},
               policy_estimators=q_vals,
               action_layers=resolver)
 
@@ -71,7 +72,7 @@ from agentnet.experiments.openai_gym.pool import GamePool
 pool = GamePool(GAME_TITLE, N_PARALLEL_GAMES)
 
 
-def update_pool(env, pool, n_steps=100):
+def update_pool(env, pool, n_steps=REPLAY_SEQ_LEN):
     preceding_memory_states = list(pool.prev_memory_states)
 
     #get interaction sessions
@@ -86,8 +87,11 @@ def update_pool(env, pool, n_steps=100):
     print(memory_logs[0].shape)"""
 
     #load them into experience replay environment
-    env.load_sessions(observation_tensor/128., action_tensor, reward_tensor,
-                      is_alive_tensor, preceding_memory_states)  # max_pool_size=1e5)
+    print("obstensor shape: ", observation_tensor[0].shape)
+    print("obs len:", len(env.observations), "obs shape: ", env.observations[0].get_value().shape)
+
+    env.append_sessions(observation_tensor/128., action_tensor, reward_tensor,
+                        is_alive_tensor, preceding_memory_states, max_pool_size=1e4)  # TODO: what does it mean?
     # TODO: make the constant global
 
 #compile theano graph for one step decision making
@@ -101,23 +105,28 @@ def step(observation, prev_memories='zeros', batch_size=N_PARALLEL_GAMES):
     if prev_memories == 'zeros':
         prev_memories = [np.zeros((batch_size,)+tuple(mem.output_shape[1:]),
                          dtype='float32') for mem in agent.agent_states]
-#    print("prev memories:", prev_memories)
+    #print("prev memories:", prev_memories)
     res = applier_fun(np.array(observation), *prev_memories)
     action = res[0]
     memories = res[1:]
 #    print("new memories:", memories)
     return action, memories
 
-batch_size = 64  # TODO: make a constant global
-#batch_env = env.sample_session_batch(batch_size)
-(state_seq,), observation_seq, agent_state, action_seq, qvalues_seq = agent.get_sessions(
-    env,
+# fill the pool
+pool.interact(step, 1)
+preceding_memory_states = list(pool.prev_memory_states)
+obs_tensor, action_tensor, reward_tensor, _, is_alive_tensor, _ = pool.interact(step, REPLAY_SEQ_LEN)
+env.load_sessions(obs_tensor/128., action_tensor, reward_tensor, is_alive_tensor, preceding_memory_states)
+
+
+batch_size = 64
+batch_env = env.sample_session_batch(batch_size)
+_, _, _, _, qvalues_seq = agent.get_sessions(
+    batch_env,
     session_length=REPLAY_SEQ_LEN,
-    batch_size=env.batch_size,
+    batch_size=batch_env.batch_size,
     optimize_experience_replay=True,
 )
-
-rewards_seq = env.get_reward_sequences(state_seq, action_seq)
 
 from agentnet.learning import qlearning
 
@@ -127,11 +136,10 @@ gamma = theano.shared(np.float32(0.95), name='q_learning_gamma')
 # TODO: scale rewards
 squared_Qerror = qlearning.get_elementwise_objective(
     qvalues_seq,
-    env.actions[0],
-    env.rewards,
-    env.is_alive,
+    batch_env.actions[0],
+    batch_env.rewards,
+    batch_env.is_alive,
     gamma_or_gammas=gamma)
-
 mse_loss = squared_Qerror.sum()
 
 from lasagne.regularization import regularize_network_params, l2
@@ -139,9 +147,9 @@ reg_l2 = regularize_network_params(resolver, l2)*10**-5
 loss = mse_loss + reg_l2
 
 weights = lasagne.layers.get_all_params(resolver, trainable=True)
-updates = lasagne.updates.adadelta(loss, weights, learning_rate=0.001)
+updates = lasagne.updates.adadelta(loss, weights, learning_rate=0.01)
 
-mean_session_reward = rewards_seq.sum(axis=1).mean()
+mean_session_reward = env.rewards.sum(axis=1).mean()
 
 train_fun = theano.function([], [loss, mean_session_reward], updates=updates)
 compute_mean_session_reward = theano.function([], mean_session_reward)
@@ -163,12 +171,12 @@ from agentnet.display import Metrics
 score_log = Metrics()
 
 epoch_counter = 1
-n_epochs = 1000
+n_epochs = 25000
 for i in range(n_epochs):
     #train
     update_pool(env, pool, REPLAY_SEQ_LEN)
-    #batch_env = env.sample_session_batch(batch_size)
     resolver.rng.seed(i)
+    batch_env = env.sample_session_batch(batch_size)
     loss, avg_reward = train_fun()
 
     ##update resolver's epsilon (chance of random action instead of optimal one)
@@ -195,12 +203,13 @@ for i in range(n_epochs):
         #back to epsilon-greedy
         resolver.epsilon.set_value(np.float32(current_epsilon))
         update_pool(env, pool, REPLAY_SEQ_LEN)
-        print(".",)
 
     if epoch_counter % 100 == 0:
         print("Learning curves:")
-        score_log.plot()
+       # score_log.plot()
 
         print("Random session examples")
-        display_sessions()
+       # display_sessions()
+        print(avg_reward_greedy)
+
     epoch_counter += 1
